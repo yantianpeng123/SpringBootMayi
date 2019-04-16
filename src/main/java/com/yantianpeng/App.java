06ad36ea362ad45005af1c422c7d28861adad315
yantianpeng <hkdyantianpeng@163.com>
2019-04-16 17:09:23 +0800

    删除不必要的东西

---
+++ src/main/java/com/yantianpeng/App.java
@@ -0,0 +1,18 @@
+package com.yantianpeng;
+
+/**
+
+* @Description:    SpringBoot启动类
+
+* @Author:        yantianpeng
+
+* @CreateDate:     2019/4/16 下午5:07
+
+
+*/
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
