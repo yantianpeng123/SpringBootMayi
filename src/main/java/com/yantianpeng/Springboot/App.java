b2066c992cb38ffd469ae1854c0e97bcf22b2b47
yantianpeng <hkdyantianpeng@163.com>
2019-04-16 17:03:37 +0800

    first commit

---
+++ src/main/java/com/yantianpeng/Springboot/App.java
@@ -0,0 +1,13 @@
+package com.yantianpeng.Springboot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
